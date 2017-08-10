5aa69d56bc8755484fc30644353a2be8bbc45afe
Mike111177 <captain@mikeddavidson.com>
2017-08-10 13:01:23 -0400

    Improved command line arguments.

--- runmynodebot.js
+++ runmynodebot.js
@@ -7,59 +7,77 @@ const path = require('path');
 var argv = require('yargs')
 	.usage('usage: sudo node $0 <robot-ID> [--opts]')
 	.options({
-		//Debug option	
-		'debug':{
-			alias: 'd',
-			describe: 'Enable server output and repl server. Not reccomended for headless operation.',
-			type: 'boolean'
-		},
 		//Drive mode option
 		'drive-mode': {
 			describe: 'Command parser used for movement motors.',
 			choices: fs.readdirSync(__dirname + path.sep + 'drive_modes').map(fname => {return fname.slice(0,-3);}), //Reading from drive_modes
 			default: 'fcfs_tank',
-			type: 'string'
+			type: 'string',
+			group: 'Driving:'
 		},
 		//Bias option
 		'bias': {
 			describe: 'Use this if your robot is lopsided, positive numbers dampen the right side motors, and vice versa. Must be between -1 and 1.',
 			default: 0,
-			type: 'number'
+			type: 'number',
+			group: 'Driving:'
 		},
 		//Speed
 		'speed': {
 			alias: 's',
 			describe: 'Base motor speed (power). Must be between 0 and 255',
 			default: 255,
-			type: 'number'
+			type: 'number',
+			group: 'Driving:'
 		},
 		'straight-time': {
 			describe: 'Set the time that the drive motors run when going straight. (Milliseconds)',
 			default: 500,
-			type: 'number'
+			type: 'number',
+			group: 'Driving:'
 		},
 		'turn-time': {
 			describe: 'Set the time that the drive motors run when turning. (Milliseconds)',
 			default: 500,
-			type: 'number'
+			type: 'number',
+			group: 'Driving:'
 		},
 		'allow-anon-tts':{
 			describe: 'Allow anonymous users to use tts without a login.',
 			default: false,
-			type:'boolean'
+			type:'boolean',
+			group: 'Text-To-Speech:'
 		},
 		'tts-driver':{
 			describe: 'Choose which tts engine to use.',
 			default: 'espeak',
 			choices: fs.readdirSync(__dirname + path.sep + 'tts_drivers').map(fname => {return fname.slice(0,-3);}), //Reading from tts_drivers
-			type: 'string'
+			type: 'string',
+			group: 'Text-To-Speech:'
 		},
 		'config': {
 			alias: 'c',
-			default: null,
 			describe: 'Path to a robot configuration file you would like to use.',
 			normalize: true,
 			type: 'string'
+		},
+		//Debug options
+		'repl':{
+			alias: ['debug', 'd'],
+			describe: 'Enable repl server. Not reccomended for headless operation.',
+			type: 'boolean',
+			group: 'Debugging:'
+		},
+		'verbose':{
+			alias: 'v',
+			describe: 'Print each command sent to robot on console.',
+			type: 'boolean',
+			group: 'Debugging:'
+		},
+		'no-connect': {
+			describe: 'Don\'t connect to letsrobot.tv. This is usefull for offline hardware debugging. Requires --repl',
+			type: 'boolean',
+			group: 'Debugging:'
 		}
 	})
 	//Check that bias is between -1 and 1
@@ -68,8 +86,6 @@ var argv = require('yargs')
 	.check(({speed}) => {if (0<=speed && speed<=255) return true; else throw(new Error('Error: Speed must be between 0 and 255.'));})
 	//Check that move times are greater than zero.
 	.check((args) => {if (0<=args['turn-time'] && 0<=args['straight-time']) return true; else throw(new Error('Error: turn-time and straight-time must both be greater than 0.'));})
-	//Check that all sound files in play are .wav files.
-	.check(({play}) => {if (Object.values(play).every((str)=>str.endsWith('.wav'))) return true; else throw(new Error('Error: sound files must be .wav files.'));})
 	//Check for valid config format
 	.check(({config}) => {if (!config || config.endsWith('.yml') || config.endsWith('.json')) return true; else throw(new Error('Error: config file must be yaml compatibale files. (.yml or .json)'));})
 	//Argument debug. --yargs
@@ -163,7 +179,7 @@ function stop(delay=500){
 
 //Motors 1 and 2 (0 and 1) are drive motors, 3 and 4 (2 and 3) are accessories.
 robot.on('command_to_robot', data => {
-	if (argv.debug){
+	if (argv.verbose){
 		console.log(data);
 	}
 	
