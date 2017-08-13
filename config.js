fdcdde3a735c2d71589d24691b7e887e97b8fd63
Mike111177 <captain@mikeddavidson.com>
2017-08-13 02:09:06 -0400

    config initialization now returns devices

--- config.js
+++ config.js
@@ -9,7 +9,6 @@ const five = require('johnny-five');
 function loadConfig(inputFile){
 	var config = yaml.safeLoad(fs.readFileSync(inputFile));
 	var hardware = setupHardware(config.devices);
-	
 	return {
 		config: config,
 		hardware: hardware
@@ -17,39 +16,42 @@ function loadConfig(inputFile){
 }
 
 function setupHardware(devices, debug){
-	var devicemap = {};
-	let board_opts = [];
-	devices.forEach(board_entry => {
-		 let opts = {id: board_entry.name || board_entry.type};
-		 Object.assign(opts, defaults(board_entry.type));
-		 board_opts.push(opts);
-	});
-	var boards = new five.Boards(board_opts);
-	boards.on('ready', ()=>{
-		devices.forEach((board_entry) => {
-			let board = this.byId(board_entry.name || board_entry.type);
-			devicemap[board.id] = board;
-			if (board_entry.parts){
-				board_entry.parts.forEach((part)=>{
-					devicemap[part.name||part.type] = initPart(part, board);
-				});
-			}
-			if (board_entry.children){
-				board_entry.children.forEach((child) => {
-					let opts = {
-							id: child.name||child.type,
-							controller: child.type,
-							board: board};
-					if (child.address) opts.address = child.address;
-					let child_device = new five.Board.Virtual(new five.Expander(opts));
-					devicemap[opts.id] = child_device;
-					if (child.parts){
-						child.parts.forEach((part)=>{
-							devicemap[part.name||part.type] = initPart(part, child_device);
-						});
-					}
-				});
-			}
+	return new Promise((resolve, reject) => {
+		var devicemap = {};
+		let board_opts = [];
+		devices.forEach(board_entry => {
+			let opts = {id: board_entry.name || board_entry.type};
+			Object.assign(opts, defaults(board_entry.type));
+			board_opts.push(opts);
+		});
+		var boards = new five.Boards(board_opts);
+		boards.on('ready', ()=>{
+			devices.forEach((board_entry) => {
+				let board = this.byId(board_entry.name || board_entry.type);
+				devicemap[board.id] = board;
+				if (board_entry.parts){
+					board_entry.parts.forEach((part)=>{
+						devicemap[part.name||part.type] = initPart(part, board);
+					});
+				}
+				if (board_entry.children){
+					board_entry.children.forEach((child) => {
+						let opts = {
+								id: child.name||child.type,
+								controller: child.type,
+								board: board};
+						if (child.address) opts.address = child.address;
+						let child_device = new five.Board.Virtual(new five.Expander(opts));
+						devicemap[opts.id] = child_device;
+						if (child.parts){
+							child.parts.forEach((part)=>{
+								devicemap[part.name||part.type] = initPart(part, child_device);
+							});
+						}
+					});
+				}
+			});
+			resolve(devicemap);
 		});
 	});
 }
@@ -67,3 +69,5 @@ function initPart(partconf, board){
 	let partClass = five[partconf.type];
 	return new partClass(opts);
 }
+
+module.exports = loadConfig;
