cb0f8d69e29942b5325645e182b77e913245304a
Mike111177 <captain@mikeddavidson.com>
2017-08-27 13:51:40 -0400

    Made util.js more legible

--- util.js
+++ util.js
@@ -2,36 +2,53 @@ const { URL } = require('url');
 const { get } = require('https');
 const fs = require('fs');
 
-module.exports = {
-	dive: (obj, path) => path.split('.').reduce((acc, v) => acc[v], obj),
-	softAssign: (...objs)=>objs.slice(1).forEach(obj=>Object.keys(obj).forEach(p=>(objs[0][p]===undefined)?objs[0][p]=obj[p]:0)),
-	jsonGrab: (link) => {
-		return new Promise((resolve, reject)=>{
-			get(new URL(link), (res) =>{
-				let rawData = '';
-				res.on('data', (chunk) => { rawData += chunk; });
-				res.on('end', ()=>{
-					resolve(JSON.parse(rawData));
-				});
-			}).on('err', reject);
-		});
-	},
-	copy: (source, target) => {
-		return new Promise((resolve, reject) => {
-			let error = false;
-			let fail = (err) => {
-				error = true;
-				reject(err);
-			}
-			let rd = fs.createReadStream(source);
-			let wr = fs.createWriteStream(target);
-			rd.on('error', fail);
-			wr.on('error', fail);
-			rd.pipe(wr);
+function dive(obj, path){
+	return path.split('.').reduce((acc, v) => acc[v], obj);
+}
 
-			wr.on('done', ()=>{
-				if (!error) resolve();
+function softAssign(...objs){
+	const target = objs[0];
+	const sources = objs.slice(1);
+	sources.forEach( source => Object.keys(source).forEach( key => {
+		if (!(key in target)){
+			target[key] = source[key];
+		}
+	}));
+}
+
+function jsonGrab(link) {
+	return new Promise((resolve, reject)=>{
+		get(new URL(link), (res) =>{
+			let rawData = '';
+			res.on('data', (chunk) => { rawData += chunk; });
+			res.on('end', ()=>{
+				resolve(JSON.parse(rawData));
 			});
+		}).on('err', reject);
+	});
+}
+
+function copy(source, target) {
+	return new Promise((resolve, reject) => {
+		let error = false;
+		let fail = (err) => {
+			error = true;
+			reject(err);
+		}
+		let rd = fs.createReadStream(source);
+		let wr = fs.createWriteStream(target);
+		rd.on('error', fail);
+		wr.on('error', fail);
+		rd.pipe(wr);
+			wr.on('done', ()=>{
+			if (!error) resolve();
 		});
-	}
+	});
+}
+
+module.exports = {
+	dive: dive,
+	softAssign: softAssign,
+	jsonGrab: jsonGrab,
+	copy: copy
 };
